cefd356bdaff2b8ff0c05485b45044d7493a9b26
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-02-11 14:31:26 +0900

    Title: 나눗셈의 나머지, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240211/나눗셈의 나머지/remainder-of-division.py
+++ 240211/나눗셈의 나머지/remainder-of-division.py
@@ -1,15 +1,18 @@
+def solution(a, b):
+  S = [0] * b
+
+  while a > 0:
+    c = a//b
+    a = c
+    d = a%b
+    S[d] += 1
+
+  sum_square = 0
+  for i in S:
+    k = pow(i, 2)
+    sum_square += k
+
+  return sum_square
+
 a, b = map(int, input().split())
-S = [0] * b
-
-while a > 1:
-  c = a//b
-  a = c
-  d = a%b
-  S[d] += 1
-
-sum_square = 0
-for i in S:
-  k = i * i
-  sum_square += k
-
-print(sum_square)
+print(solution(a, b))
