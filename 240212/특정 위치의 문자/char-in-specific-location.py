6d41792fdcb5c6810c925b415f3685c0ecdfe06c
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-02-12 07:44:39 +0900

    Title: 특정 위치의 문자, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/특정 위치의 문자/char-in-specific-location.py
+++ 240212/특정 위치의 문자/char-in-specific-location.py
@@ -1,7 +1,10 @@
 x = input()
 word_list = ['L','E','B','R','O','S']
 
-if x in word_list:
-  print(word_list.index(x))
-else:
-  print('None')
+def find_index(a) : 
+  if a in word_list:
+    print(word_list.index(x))
+  else:
+    print('None')
+
+find_index(x)
