b24ad79232279c526273b353f3035055df0264e5
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-02-12 23:47:52 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 141ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240212/중복되지 않는 숫자 중 최대/max-of-unique-number.py
+++ 240212/중복되지 않는 숫자 중 최대/max-of-unique-number.py
@@ -1,17 +1,15 @@
-N = int(input())
+from collections import deque
+N =int(input())
 
 a = [int(x) for x in input().split()]
 
+b = set(a)
+myDeque = deque(b)
 
-def solution(x):
-    count = 0
-    for i in x:
-        if i == max(x):
-            count += 1
-    
-    if count == 1:
-        return max(a)
-    else:
-        return -1
-    
-print(solution(a))
+
+while len(myDeque) > 0 and a.count(myDeque[-1]) >= 2:
+  myDeque.pop()
+if len(myDeque) > 0 and a.count(myDeque[-1]) == 1:
+  print(myDeque[-1])
+else:
+  print(-1)
