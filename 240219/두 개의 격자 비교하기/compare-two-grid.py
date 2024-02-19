02677f6561f094a87861f927b4ec46d5c83aefbe
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-02-19 12:45:47 +0900

    Title: 두 개의 격자 비교하기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240219/두 개의 격자 비교하기/compare-two-grid.py
+++ 240219/두 개의 격자 비교하기/compare-two-grid.py
@@ -1,26 +1,17 @@
 n, m = map(int, input().split())
 
-a = [
-  list(map(int, input().split()))
-  for _ in range(m)  
-]
+a = [list(map(int, input().split())) for _ in range(n)]
 
-b = [
-  list(map(int, input().split()))
-  for _ in range(m)  
-]
+b = [list(map(int, input().split())) for _ in range(n)]
 
-new_grid = [
-  [0 for _ in range(n)]  
-  for _ in range(m) 
-]
+new_grid = [[0 for _ in range(m)] for _ in range(n)] 
 
-for i in range(m):  
-  for j in range(n):  
-    if a[i][j] == b[i][j]:
-      new_grid[i][j] = 0
-    else:
-      new_grid[i][j] = 1
+for i in range(n):
+    for j in range(m):
+        if a[i][j] == b[i][j]:
+            new_grid[i][j] = 0
+        else:
+            new_grid[i][j] = 1
 
-for row in new_grid: 
-  print(' '.join(map(str, row)))
+for row in new_grid:
+    print(' '.join(map(str, row)))
