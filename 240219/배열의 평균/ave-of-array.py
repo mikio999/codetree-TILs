1dc76e14261b4413eab1862490acd616a2117b80
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-02-19 11:25:40 +0900

    Title: 배열의 평균, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240219/배열의 평균/ave-of-array.py
+++ 240219/배열의 평균/ave-of-array.py
@@ -8,4 +8,4 @@ for i in range(4):
   print((grid[0][i] + grid[1][i])/2, end = ' ')
 print()
 
-print(((sum(grid[0])/4) + (sum(grid[1])/4))/2)
+print(round(((sum(grid[0])/4) + (sum(grid[1])/4))/2, 1))
