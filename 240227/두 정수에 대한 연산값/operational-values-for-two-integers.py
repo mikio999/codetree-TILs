c7727091766b7b95321be05da0ac1e75e64fc25b
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-02-27 22:23:02 +0900

    Title: 두 정수에 대한 연산값, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/두 정수에 대한 연산값/operational-values-for-two-integers.py
+++ 240227/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -1,12 +1,12 @@
 a, b = map(int, input().split())
 def calculate_two_int(a,b) :
     if a > b :
-        first_num = a + 25
-        second_num = b * 2
+        b *= 2
+        a += 25
     else :
-        first_num = a * 2
-        second_num = b + 25
-      
-    print('{} {} '.format(first_num, second_num), end = '') 
+        b += 25
+        a *= 2
+    return a, b
 
-calculate_two_int(a,b)
+a, b =calculate_two_int(a,b)
+print(a,b)
