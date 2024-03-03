6f78939c8cf929cbad71de80e6184da5762becb3
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-03 12:20:02 +0900

    Title: 큰 숫자 자리수의 합, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
+++ 240303/큰 숫자 자리수의 합/sum-of-large-numeric-digits.py
@@ -1,13 +1,8 @@
-a = [int(x) for x in input().split()]
+a, b, c = tuple(map(int, input().split()))
 
-def f(a):
-  if len(a) == 1:
-    return a[0]
-  return f(a[1:]) * a[0]
+def digit_sum(n):
+  if n < 10:
+    return n
+  return digit_sum(n//10) + n%10
 
-result = str(f(a))
-
-num = 0
-for i in range(len(result)):
-  num += int(result[i])
-print(num)
+print(digit_sum(a*b*c))
