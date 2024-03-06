d9805757149bf76d91dd3c5548fa27638e3df8ef
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-06 21:50:15 +0900

    Title: 중앙값 계산 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240306/중앙값 계산 2/get-median-2.py
+++ 240306/중앙값 계산 2/get-median-2.py
@@ -1,7 +1,15 @@
 n = int(input())
 a = [int(x) for x in input().split()]
+b = []
+ans = []
+for i in range(n+1):
+  if i % 2 == 1:
+    b.append(a[:i])
+    b.sort()
 
-a.sort()
+for j in range(len(b)):
+  b[j].sort()
+  ans.append(b[j][j])
 
-for i in range(n//2 + 1):
-  print(a[i], end=' ')
+for _ in ans:
+  print(_, end = ' ')
