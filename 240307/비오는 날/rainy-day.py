232d3749dd8c2c619b072f1dd78266b03a864eaa
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-07 11:33:26 +0900

    Title: 비오는 날, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/비오는 날/rainy-day.py
+++ 240307/비오는 날/rainy-day.py
@@ -1,29 +1,20 @@
 class Forecast:
-  def __init__(self, day, date, weather) :
-    self.day = day
+  def __init__(self, date, day, weather) :
     self.date = date
+    self.day = day
     self.weather = weather
 
 n = int(input())
 weather_list = []
-for _ in range(n):
-  date, day, weather = tuple(input().split())
-  weather_list.append(Forecast(day, date, weather))
-
 rain_list = []
+
+answer = Forecast('2100-99-99','','')
 for i in range(n):
+  date, day, weather = tuple(input().split())
+  weather_list.append(Forecast(date,day, weather))
+
   if weather_list[i].weather == 'Rain':
-    rain_list.append(weather_list[i])
+    if answer.date >= weather_list[i].date:
+      answer = Forecast(date,day,weather)
 
-target = 0
-for j in range(1,len(rain_list)):
-  if int(rain_list[target].date[0:4]) > int(rain_list[j].date[0:4]) :
-    target = j
-  elif int(rain_list[target].date[0:4]) == int(rain_list[j].date[0:4]) :
-    if int(rain_list[target].date[5:7]) == int(rain_list[j].date[5:7]) :
-      if int(rain_list[target].date[8:10]) > int(rain_list[j].date[8:10]) :
-        target = j
-    elif int(rain_list[target].date[5:7]) > int(rain_list[j].date[5:7]) :
-      target = j
-
-print(rain_list[target].date, rain_list[target].day, rain_list[target].weather)
+print(answer.date, answer.day, answer.weather)
