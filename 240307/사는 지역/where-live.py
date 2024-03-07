14c0c57fa10335510938b1086ba7baca047bd9a6
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-07 10:59:32 +0900

    Title: 사는 지역, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240307/사는 지역/where-live.py
@@ -0,0 +1,19 @@
+class Address:
+  def __init__(self, name, number, location):
+    self.name = name
+    self.number = number
+    self.location =location
+
+n = int(input())
+
+address_list = []
+for i in range(n) :
+  name, number, location = tuple(input().split())
+  address_list.append(Address(name, number, location))
+
+for j in range(1,n):
+  target = 0
+  if address_list[j].name > address_list[target].name :
+    target = j
+
+print(f'name {address_list[target].name}\naddr {address_list[target].number}\ncity {address_list[target].location}')
