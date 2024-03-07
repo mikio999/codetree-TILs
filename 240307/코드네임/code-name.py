b003ab51845228fba78d23c9614ec0f579064abc
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-07 10:26:43 +0900

    Title: 코드네임, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/코드네임/code-name.py
+++ 240307/코드네임/code-name.py
@@ -16,6 +16,5 @@ for i in range(1,5):
   if int(spy_list[i].score) < min_num:
     target = i
     min_num = int(spy_list[i].score)
-    print('target',target,'min_num',min_num)
 
 print(spy_list[target].name, spy_list[target].score)
