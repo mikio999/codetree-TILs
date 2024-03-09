f5be6b1a99f627026f6439506813edf06fc5d527
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-09 17:19:03 +0900

    Title: 그 요일은, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240309/그 요일은/the-day-of-the-day.py
+++ 240309/그 요일은/the-day-of-the-day.py
@@ -1,4 +1,4 @@
-m1, d1, m2, d2 = tuple(map(int, input().split()))
+m1, d1, m2, d2 = map(int, input().split())
 A = input()
 
 days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
@@ -9,15 +9,21 @@ distance = 0
 count = 0
 
 while True:
-  if month == m2 and day == d2+1:
-    break
-  else:
-    day += 1
-    distance += 1
+  if month == m2 and day == d2:
     if days[distance%7] == A:
       count += 1
+    day += 1
+    distance += 1
+    break
+  else:
+    if days[distance%7] == A:
+      count += 1
+    day += 1
+    distance += 1
+      
     if day == day_list[month]:
       month += 1
       day = 1
+      distance += 1
 
 print(count)
