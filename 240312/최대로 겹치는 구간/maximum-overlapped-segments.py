633bd2470149bbf67fb1cd16e19f0fe978b800b4
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-12 12:33:43 +0900

    Title: 최대로 겹치는 구간, Time: 108ms, Memory: 25MB, Status: Passed - CodeTree

--- 240312/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 240312/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,3 +1,21 @@
+OFFSET = 100
+MAX_R = 200
+
+n = int(input())
+segments = [
+  tuple(map(int,input().split())) for _ in range(n)
+]
+
+checked = [0] * (MAX_R + 1)
+
+for x1, x2 in segments :
+  x1, x2 = x1 + OFFSET, x2 + OFFSET
+  for i in range(x1, x2) :
+     checked[i] += 1
+
+print(max(checked))
+
+'''
 n = int(input())
 line_dots = []
 x2_dots = []
@@ -25,3 +43,5 @@ for i in line_dots:
 
 
 print(max(lines))
+
+'''
