9435243c9c7edecbaf86fa29d83ee2405f7706db
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-12 11:44:54 +0900

    Title: 블럭쌓는 명령2, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240312/블럭쌓는 명령2/block-stacking-commands2.py
+++ 240312/블럭쌓는 명령2/block-stacking-commands2.py
@@ -3,7 +3,7 @@ blocks = [0 for _ in range(N)]
 
 for i in range(K):
   a,b = map(int, input().split())
-  while a < b+1:
-    blocks[a] += 1
-    a += 1
+  for j in range(a-1,b):
+    blocks[j] += 1
+
 print(max(blocks))
