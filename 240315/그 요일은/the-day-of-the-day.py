36792f4fb5897574017fc2f9ae77906d1f0d63e5
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-15 12:16:14 +0900

    Title: 그 요일은, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240315/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,39 @@
+m1, d1, m2, d2 = map(int,input().split())
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+day_list = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+A = input()
+
+distance = 0
+
+while True:
+  if days[distance] == A:
+    break
+  else:
+    distance += 1
+
+month = m1
+day = d1  + distance
+elapsed_day = 0
+count = 0
+
+if m1 == m2 and d2 - d1 < distance:
+    count = 0
+else:
+    day = d1
+    while True:
+        if month == m2 and day == d2:
+           if elapsed_day >= distance:
+              elapsed_day += 1
+              break
+           else :
+              break
+        else: 
+          day += 1
+          elapsed_day += 1
+          if day > day_list[month]:
+              month += 1
+              day = 1
+              elapsed_day += 1
+
+print(elapsed_day//7)
