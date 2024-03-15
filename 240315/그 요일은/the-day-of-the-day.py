3a6535f7f2f29f6b1d3deaa52a5cea43aa9f0d08
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-15 12:50:30 +0900

    Title: 그 요일은, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240315/그 요일은/the-day-of-the-day.py
+++ 240315/그 요일은/the-day-of-the-day.py
@@ -23,18 +23,18 @@ else:
     day = d1
     while True:
         if month == m2 and day == d2:
-           elapsed_day += 1
            if elapsed_day%7 >= distance:
               count += 1
               break
            else :
               break
         else: 
-          day += 1
-          elapsed_day += 1
           if day == day_list[month]:
               month += 1
               day = 1
-
+              elapsed_day += 1
+          else:
+              day += 1
+              elapsed_day += 1
 
 print(elapsed_day//7 + count)
