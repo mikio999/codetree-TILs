ba20b2f5fa1c0a1aec3a6c0a6ab11bbbd019d41c
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-18 10:46:14 +0900

    Title: 왔다 갔던 구역 2, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240318/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240318/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -6,42 +6,25 @@ for _ in range(n) :
   x = int(x)
   commands.append((x,y))
 
-MAX_R = 201
+MAX_R = 1000
 index = MAX_R // 2
-grid = [[0] for _ in range(MAX_R)]
+grid = [0 for _ in range(MAX_R)]
 
-for command in commands :
-  if command[1] == 'R':
-    for i in range(index, index+command[0]+1) :
-      grid[i][0] += 1
-    index += command[0]
-  elif command[1] == 'L':
-    for i in range(index, index - command[0]-1, -1) :
-      grid[i][0] += 1
-    index -= command[0]
 
-dots = []
+for x, direction in commands:
+    if direction == 'R':
+        for i in range(index, index + x):
+            grid[i] += 1
+        index += x
+    else: 
+        for i in range(index - 1, index - x - 1, -1):
+            grid[i] += 1
+        index -= x
 
-for i in range(len(grid)):
-  if grid[i][0] >= 2 :
-    dots.append(i)
+revisited = 0
 
-answer = 0
+for i in grid:
+    if i > 1:
+        revisited += 1
 
-result = []
-sequence =[dots[0]]
-
-for i in range(1, len(dots)):
-  if dots[i] == dots[i-1] + 1:
-    sequence.append(dots[i])
-  else:
-    result.append(sequence)
-    sequence = [dots[i]]
-
-result.append(sequence)
-
-answer = 0
-for i in result:
-  answer += len(i) - 1
-
-print(answer)
+print(revisited)
