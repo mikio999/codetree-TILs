c07350cf69b48cf6f66a916fcb7df9260787629a
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-19 11:58:49 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240319/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240319/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -17,39 +17,42 @@ k1 += OFFSET
 m2 += OFFSET
 k2 += OFFSET
 
-first_square = []
-for i in range(x1,x2) :
-  for j in range(y1,y2) :
-    grid[i][j] += 1
-    first_square.append((i,j))
+if (x1, y1, x2, y2) == (m1, k1, m2, k2) :
+  print(0)
+else: 
+  first_square = []
+  for i in range(x1,x2) :
+    for j in range(y1,y2) :
+      grid[i][j] += 1
+      first_square.append((i,j))
 
-for i in range(m1, m2):
-  for j in range(k1, k2):
-    grid[i][j] += 1
+  for i in range(m1, m2):
+    for j in range(k1, k2):
+      grid[i][j] += 1
 
-dots = []
+  dots = []
 
-for i in range(MAX_R):
-  for j in range(MAX_R):
-    if grid[i][j] == 2 :
-      dots.append((i,j))
+  for i in range(MAX_R):
+    for j in range(MAX_R):
+      if grid[i][j] == 2 :
+        dots.append((i,j))
 
-remain_square = []
+  remain_square = []
 
-for dot in dots:
-  first_square.remove(dot)
+  for dot in dots:
+    first_square.remove(dot)
 
 
-new_dots = [[0] * MAX_R for _ in range(MAX_R)]
+  new_dots = [[0] * MAX_R for _ in range(MAX_R)]
 
-x_dots = []
-y_dots = []
+  x_dots = []
+  y_dots = []
 
-for e in first_square:
-  x_dots.append(e[0])
-  y_dots.append(e[1])
+  for e in first_square:
+    x_dots.append(e[0])
+    y_dots.append(e[1])
 
-x_length = max(x_dots) - min(x_dots) + 1
-y_length = max(y_dots) - min(y_dots) + 1
+  x_length = max(x_dots) - min(x_dots) + 1
+  y_length = max(y_dots) - min(y_dots) + 1
 
-print(x_length * y_length)
+  print(x_length * y_length)
