bed52dec630ee8c03d067e7b3c138b8d04ef5859
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-19 12:12:46 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240319/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240319/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,53 +1,28 @@
-MAX_R = 2000
-OFFSET = 1000
+def calculate_area(x1,y1,x2,y2,m1,k1,m2,k2):
+  overlap_x1 = max(x1,m1)
+  overlap_y1 = max(y1,k1)
+  overlap_x2 = min(m2,k2)
+  overlap_y2 = min(y2,k2)
 
-grid = [[0] * MAX_R for _ in range(MAX_R)]
+  overlap_width = max(0, overlap_x2 - overlap_x1)
+  overlap_height = max(0, overlap_y2 - overlap_y1)
+  overlap_area = overlap_width * overlap_height
+
+  first_area = (x2-x1) * (y2-y1)
+  remaining_area = first_area - overlap_area
+  return remaining_area
 
 x1,y1,x2,y2 = map(int, input().split())
+m1,k1,m2,k2 = map(int, input().split())
 
+OFFSET = 1000
 x1 += OFFSET
 y1 += OFFSET
 x2 += OFFSET
 y2 += OFFSET
-
-m1, k1, m2, k2 = map(int, input().split())
-
 m1 += OFFSET
 k1 += OFFSET
 m2 += OFFSET
 k2 += OFFSET
 
-first_square = []
-for i in range(x1,x2) :
-  for j in range(y1,y2) :
-    grid[i][j] += 1
-    first_square.append((i,j))
-
-for i in range(m1, m2):
-  for j in range(k1, k2):
-    grid[i][j] += 1
-
-dots = []
-
-for i in range(MAX_R):
-  for j in range(MAX_R):
-    if grid[i][j] == 2 :
-      dots.append((i,j))
-
-for dot in dots:
-  first_square.remove(dot)
-
-if len(first_square) == 0 :
-  print(0)
-else : 
-  x_dots = []
-  y_dots = []
-
-  for e in first_square:
-    x_dots.append(e[0])
-    y_dots.append(e[1])
-
-  x_length = max(x_dots) - min(x_dots) + 1
-  y_length = max(y_dots) - min(y_dots) + 1
-
-  print(x_length * y_length)
+print(calculate_area(x1, y1, x2, y2, m1, k1, m2, k2))
