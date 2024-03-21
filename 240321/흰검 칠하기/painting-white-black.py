d0d886578359bbbb191f2748afdffa52785238fd
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-21 17:26:46 +0900

    Title: 흰검 칠하기, Time: 171ms, Memory: 27MB, Status: Passed - CodeTree

--- 240321/흰검 칠하기/painting-white-black.py
+++ 240321/흰검 칠하기/painting-white-black.py
@@ -6,7 +6,7 @@ for _ in range(n):
   num = int(num)
   commands.append((num,dir))
 
-MAX_R = 100000
+MAX_R = 100001
 line = [''] * MAX_R
 
 start = MAX_R//2
@@ -28,10 +28,23 @@ gray = 0
 
 for dot in line:
   if len(dot) >= 4 :
-    if 'BW' or 'WB' in dot:
-     gray += 1
+    b_count = 0
+    w_count = 0
+    for color in dot:
+      if color == 'B':
+        b_count += 1
+      elif color == 'W':
+        w_count += 1
+    if b_count >= 2 and w_count >= 2 :
+      gray += 1
+    else :
+      if dot[-1] == 'B':
+        black += 1
+      elif dot[-1] == 'W':
+        white += 1
+    
   else:
-    if len(dot) >= 1:
+    if 1 <= len(dot) < 4:
       if dot[-1] == 'B':
         black += 1
       elif dot[-1] == 'W':
