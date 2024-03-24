9f9780c96ffe77e2ef5fa4653195a348ee7d004c
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-24 16:26:25 +0900

    Title: 연속되는 수 3, Time: 215ms, Memory: 25MB, Status: Passed - CodeTree

--- 240324/연속되는 수 3/continuous-number3.py
+++ 240324/연속되는 수 3/continuous-number3.py
@@ -1,42 +1,15 @@
 n = int(input())
-numbers = []
+numbers = [int(input()) for _ in range(n)]
 
-for i in range(n) :
-   x = int(input())
-   numbers.append(x)
-
-# n = 8
-# numbers = [2,-1,-5,-2,-3,5,8,-9]
-
-sequence_list = []
-k = []
-index = 0
-
+answer = 0
+count = 0
 
 for i in range(n):
-  if i == n-1:
-    if numbers[i] * numbers[i-1] > 0:
-      k.append(numbers[i])
-      sequence_list.append(k)
-    else:
-      sequence_list.append(k)
-      k = []
-      k.append(numbers[i])
-      sequence_list.append(k)
-  elif numbers[i] > 0:
-    k.append(numbers[i])
-    if numbers[i+1] < 0:
-      sequence_list.append(k)
-      k = []
-  elif numbers[i] < 0:
-    k.append(numbers[i])
-    if numbers[i+1] > 0:
-      sequence_list.append(k)
-      k = []
+  if i >= 1 and numbers[i] * numbers[i-1] > 0:
+    count += 1
+  else:
+    count = 1
+  
+  answer = max(answer,count)
 
-length_list = []
-
-for sequence in sequence_list:
-  length_list.append(len(sequence))
-
-print(max(length_list))
+print(answer)
