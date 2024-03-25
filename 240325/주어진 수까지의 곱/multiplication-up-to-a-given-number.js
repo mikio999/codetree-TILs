3677a56ef603acb7a2b4ca5f1d5ee62448fbb3d1
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-25 22:50:13 +0900

    Title: 주어진 수까지의 곱, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

--- 240325/주어진 수까지의 곱/multiplication-up-to-a-given-number.js
+++ 240325/주어진 수까지의 곱/multiplication-up-to-a-given-number.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split(' ')
-let a = input[0]
-let b = input[1]
+
+let a = Number(input[0])
+let b = Number(input[1])
 let prod = 1
 
 for (i = a; i <= b; i++) {
     prod *= i
 }
+
 console.log(prod)
