544329a78fce595bf9605336942d1335be8f26a6
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-25 11:40:21 +0900

    Title: 연속되는 수 4, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/연속되는 수 4/continuous-number4.py
+++ 240325/연속되는 수 4/continuous-number4.py
@@ -1,14 +1,14 @@
 n = int(input())
 numbers=[int(input()) for _ in range(n)]
 
-cnt = 1
+cnt = 0
 ans = 0
 
 for i in range(n):
   if i==0 or numbers[i] > numbers[i-1]:
     cnt += 1
-    ans = max(ans,cnt)
   else :
     cnt = 1
+  ans = max(ans,cnt)
 
 print(ans)
