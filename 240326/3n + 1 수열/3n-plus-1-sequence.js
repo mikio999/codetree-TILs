f9f9e01170f86913fbf436f4fb5308e16c69bf87
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-26 10:00:38 +0900

    Title: 3n + 1 수열, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 240326/3n + 1 수열/3n-plus-1-sequence.js
+++ 240326/3n + 1 수열/3n-plus-1-sequence.js
@@ -5,18 +5,16 @@ let n = Number(input)
 let cnt = 0
 
 while (true) {
-
     if (n%2==0){
         n = n/2
-        cnt += 1
     }
     else if (n===1) {
         break
     }
     else {
         n = 3*n + 1
-        cnt += 1
     }
+    cnt++
 }
 
 console.log(cnt)
