4d08483bb8cbb489bf12daffb612e8d5940e02d2
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-03-29 19:59:26 +0900

    Title: 점수대 파악하기, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

--- 240329/점수대 파악하기/find-out-the-score-range.js
+++ 240329/점수대 파악하기/find-out-the-score-range.js
@@ -1,42 +1,14 @@
 const fs =require('fs')
 const input = fs.readFileSync(0).toString().split(' ').map(Number)
-let cnt = [0,0,0,0,0,0,0,0,0,0]
 
-for (let i = 0; i < input.length; i++ ) {
-    if (input[i] > 10) {
-        if (input[i] === 100) {
-            cnt[0] += 1
-        }
-        else if (input[i] >= 90) {
-            cnt[1] += 1
-        }
-        else if (input[i] >= 80) {
-            cnt[2] += 1
-        }
-        else if (input[i] >= 70) {
-            cnt[3] += 1
-        }
-        else if (input[i] >= 60) {
-            cnt[4] += 1
-        }
-        else if (input[i] >= 50) {
-            cnt[5] += 1
-        }
-        else if (input[i] >= 40) {
-            cnt[6] += 1
-        }
-        else if (input[i] >= 30) {
-            cnt[7] += 1
-        }
-        else if (input[i] >= 20) {
-            cnt[8] += 1
-        }
-        else if (input[i] >= 10) {
-            cnt[9] += 1
-        }
-    }
+let cnt = [0,0,0,0,0,0,0,0,0,0,0]
+
+for (let i = 0; i < 100; i++) {
+    if (input[i] === 0) break;
+    if (input[i] < 10) continue;
+    cnt[parseInt(input[i] /10)]++;
 }
 
-for (let j=0; j<10; j++) {
-    console.log(`${100-(j*10)} - ${cnt[j]}`)
+for (let i = 10; i >= 1; i--) {
+    console.log(i + "0 - " + cnt[i])
 }
