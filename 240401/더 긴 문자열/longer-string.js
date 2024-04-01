0820f6a239e215294672b9f3843ba151cac35903
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-04-01 11:34:26 +0900

    Title: 더 긴 문자열, Time: 222ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 240401/더 긴 문자열/longer-string.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(' ')
+
+if (input[0].length > input[1].length) {
+    console.log(input[0], input[0].length)
+}
+
+else if (input[0].length < input[1].length) {
+    console.log(input[1], input[1].length)
+}
+
+else {
+    console.log('same')
+}
