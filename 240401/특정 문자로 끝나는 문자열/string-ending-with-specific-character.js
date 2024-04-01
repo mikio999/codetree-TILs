bf3608f5876553d1010858d5fbd75a481ace2404
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-04-01 12:06:37 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 262ms, Memory: 14MB, Status: Passed - CodeTree

--- 240401/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 240401/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -6,6 +6,15 @@ const answer = []
 for (let i = 0; i < 10; i++ ) {
     let strLength = input[i].length
     if (input[i][strLength-1] === target) {
-        console.log(input[i])
+        answer.push(input[i])
     }
+}
+
+if (answer.length) {
+    for (let i = 0; i < answer.length; i++ ) {
+        console.log(answer[i])
+    }
+}
+else {
+    console.log('None')
 }
