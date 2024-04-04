db8e06536d41e874fa22a167ea0d84163139e288
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-04-04 23:55:20 +0900

    Title: 규칙에 따라 밀기, Time: 215ms, Memory: 14MB, Status: Passed - CodeTree

--- 240404/규칙에 따라 밀기/push-by-the-rules.js
+++ 240404/규칙에 따라 밀기/push-by-the-rules.js
@@ -5,10 +5,10 @@ const command = input[1]
 
 for (let i = 0; i < command.length; i++ ) {
     if (command[i] === 'L') {
-        A = A.slice(1) + A[0]
+        A = A.slice(1) + A.slice(0,1)
     }
     else if (command[i] === 'R') {
-        A = A[A.length - 1] + A.slice(0, -1)
+        A = A.slice(-1) + A.slice(0, -1)
     }
 }
 
