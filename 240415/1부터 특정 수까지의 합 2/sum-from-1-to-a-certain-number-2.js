ea40dd41285da9c124e17e1b859ac31119911c99
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-04-15 17:23:04 +0900

    Title: 1부터 특정 수까지의 합 2, Time: 259ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240415/1부터 특정 수까지의 합 2/sum-from-1-to-a-certain-number-2.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const N = Number(fs.readFileSync(0).toString().trim())
+
+function f(n) {
+    if (n === 1) {
+        return 1;
+    }
+    return f(n-1) + n;
+}
+
+console.log(f(N))
