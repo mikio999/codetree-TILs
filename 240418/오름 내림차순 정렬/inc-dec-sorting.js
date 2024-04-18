775f3872cd1aa86c9968cc027e3f338bc99bfa6d
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-04-18 16:47:05 +0900

    Title: 오름 내림차순 정렬, Time: 223ms, Memory: 14MB, Status: Passed - CodeTree

--- 240418/오름 내림차순 정렬/inc-dec-sorting.js
+++ 240418/오름 내림차순 정렬/inc-dec-sorting.js
@@ -4,22 +4,9 @@ const input = fs.readFileSync(0).toString().trim().split(`\n`)
 const n = Number(input[0])
 const numbers = input[1].split(' ').map(Number)
 
-function cmp(prev, cur) {
-    return prev - cur;
-}
 
-numbers.sort(cmp)
+numbers.sort((a,b) => a - b)
 
-let str1 = ''
-for (let i = 0; i < n; i++) {
-    str1 += numbers[i] + ' '
-}
+console.log(numbers.join(' '))
 numbers.reverse()
-let str2 = ''
-for (let i = 0; i < n; i++) {
-    str2 += numbers[i] + ' '
-}
-
-
-console.log(str1)
-console.log(str2)
+console.log(numbers.join(' '))
