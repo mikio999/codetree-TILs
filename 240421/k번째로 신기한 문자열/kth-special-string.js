4aed6956a411c6576abca3e0d1bebfb3d40b6349
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-04-21 18:12:56 +0900

    Title: k번째로 신기한 문자열, Time: 179ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240421/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(`\n`)
+
+const first = input[0].split(' ')
+
+const n = Number(first[0])
+const k = Number(first[1])
+const T = first[2]
+
+let TList = []
+
+for (let i = 1; i < input.length; i++ ) {
+    if (T === input[i].slice(0,2)) {
+        TList.push(input[i])
+    }
+}
+
+TList.sort()
+
+console.log(TList[k-1])
