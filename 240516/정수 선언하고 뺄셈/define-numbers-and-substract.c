38229c3b86e024b67dbb450ba3af85aa1f92da35
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-05-16 13:18:26 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240516/정수 선언하고 뺄셈/define-numbers-and-substract.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 97;
+    int b = 13;
+    int c = a - b;
+    printf("%d",a);
+    printf(" - ");
+    printf("%d",b);
+    printf(" = ");
+    printf("%d", c);
+
+    return 0;
+}
