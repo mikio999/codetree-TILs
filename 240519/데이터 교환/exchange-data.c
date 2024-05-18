7ba6a55c782868fbba322b2a8b7d121a1e5f7714
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-05-19 08:37:47 +0900

    Title: 데이터 교환, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240519/데이터 교환/exchange-data.c
+++ 240519/데이터 교환/exchange-data.c
@@ -2,17 +2,12 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a = 5;
-    int b = 6;
-    int c = 7;
-    int temp;
-    int temp2;
+    int a = 5, b = 6, c = 7;
 
-    temp = c;
-    temp2 = b;
-    b = a;
-    c = temp2;
-    a = temp;
+    int temp = a;
+    a = c;
+    c = b;
+    b = temp;
 
     printf("%d\n%d\n%d\n", a, b, c);
     return 0;
