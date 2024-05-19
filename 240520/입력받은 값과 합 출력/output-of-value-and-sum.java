f93afb6ecbb6cff1856c30db6e414a344c41145e
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-05-20 08:53:37 +0900

    Title: 입력받은 값과 합 출력, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 여기에 코드를 작성해주세요.
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
