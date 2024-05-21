e649add82bae4bcb9751c2dd2e978ccf31731177
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-05-21 10:00:49 +0900

    Title: 입력받아 출력 2, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240521/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 여기에 코드를 작성해주세요.
+        sc.useDelimiter("-");
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
