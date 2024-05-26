bb209223f3066b6be0f488928d55c41af3787836
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-05-26 20:06:18 +0900

    Title: 큰 수에서 빼기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240526/큰 수에서 빼기/subtract-from-large-number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    // 여기에 코드를 작성해주세요.
+    scanf("%d %d",&a, &b);
+    if (a > b) {
+        printf("%d", a-b);
+    }
+    if (a < b) {
+        printf("%d", b-a);
+    }
+    else {
+        printf(0);
+    }
+    return 0;
+}
