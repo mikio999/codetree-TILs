3bcd0424435955f895c3704105a283b2a7e7dd2e
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-08 14:42:20 +0900

    Title: 거품 정렬 구현, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240808/거품 정렬 구현/implement-bubble-sort.js
+++ 240808/거품 정렬 구현/implement-bubble-sort.js
@@ -3,19 +3,38 @@ let a = fs.readFileSync(0).toString().split(`\n`)
 let n = parseInt(a[0]);
 let numbers = a[1].split(' ').map(Number)
 
-let isSorted = true;
+// let isSorted = true;
 
-do {
-    isSorted = true;
-    let temp
-    for (let i = 0; i < n-1; i++) {
-        if (numbers[i] > numbers[i+1]) {
-            temp = numbers[i];
-            numbers[i] = numbers[i+1];
-            numbers[i+1] = temp;
-            isSorted = false
+// do {
+//     isSorted = true;
+//     let temp
+//     for (let i = 0; i < n-1; i++) {
+//         if (numbers[i] > numbers[i+1]) {
+//             temp = numbers[i];
+//             numbers[i] = numbers[i+1];
+//             numbers[i+1] = temp;
+//             isSorted = false
+//         }
+//     }
+// } while (isSorted === false)
+
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                // 두 요소를 교환합니다.
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
         }
-    }
-} while (isSorted === false)
+        n--;
+    } while (swapped);
+    return arr;
+}
 
-console.log(numbers.join(' '))
+console.log(bubbleSort(numbers).join(' '))
