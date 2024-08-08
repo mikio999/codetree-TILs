97f6409551e6a4cd9f05d75a2cec3c18b66f5b1d
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-08 14:44:49 +0900

    Title: 거품 정렬 구현, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

--- 240808/거품 정렬 구현/implement-bubble-sort.js
+++ 240808/거품 정렬 구현/implement-bubble-sort.js
@@ -1,40 +1,48 @@
-const fs = require('fs');
-let a = fs.readFileSync(0).toString().split(`\n`)
-let n = parseInt(a[0]);
-let numbers = a[1].split(' ').map(Number)
+// const fs = require('fs');
+// let a = fs.readFileSync(0).toString().split(`\n`)
+// let numbers = a[1].split(' ').map(Number)
 
-// let isSorted = true;
-
-// do {
-//     isSorted = true;
-//     let temp
-//     for (let i = 0; i < n-1; i++) {
-//         if (numbers[i] > numbers[i+1]) {
-//             temp = numbers[i];
-//             numbers[i] = numbers[i+1];
-//             numbers[i+1] = temp;
-//             isSorted = false
+// function bubbleSort(arr) {
+//     let n = arr.length;
+//     let swapped;
+//     do {
+//         swapped = false;
+//         for (let i = 0; i < n - 1; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i];
+//                 arr[i] = arr[i + 1];
+//                 arr[i + 1] = temp;
+//                 swapped = true;
+//             }
 //         }
-//     }
-// } while (isSorted === false)
+//         n--;
+//     } while (swapped);
+//     return arr;
+// }
 
-function bubbleSort(arr) {
-    let n = arr.length;
-    let swapped;
-    do {
-        swapped = false;
+// console.log(bubbleSort(numbers).join(' '))
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+// 변수 선언 및 입력
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+
+function bubbleSort() {
+    let isSorted = false;
+    while (!isSorted) {
+        isSorted = true;
         for (let i = 0; i < n - 1; i++) {
             if (arr[i] > arr[i + 1]) {
-                // 두 요소를 교환합니다.
-                let temp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = temp;
-                swapped = true;
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+                isSorted = false;
             }
         }
-        n--;
-    } while (swapped);
-    return arr;
+    }
 }
 
-console.log(bubbleSort(numbers).join(' '))
+bubbleSort();
+
+// 정렬된 배열 요소 출력
+console.log(arr.join(' '));
