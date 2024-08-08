b6a5720b65ed83546e41132e1e1272bdc6077686
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-08 14:45:34 +0900

    Title: 거품 정렬 구현, Time: 215ms, Memory: 14MB, Status: Passed - CodeTree

--- 240808/거품 정렬 구현/implement-bubble-sort.js
+++ 240808/거품 정렬 구현/implement-bubble-sort.js
@@ -1,48 +1,23 @@
-// const fs = require('fs');
-// let a = fs.readFileSync(0).toString().split(`\n`)
-// let numbers = a[1].split(' ').map(Number)
+const fs = require('fs');
+let a = fs.readFileSync(0).toString().trim().split(`\n`)
+let numbers = a[1].split(' ').map(Number)
 
-// function bubbleSort(arr) {
-//     let n = arr.length;
-//     let swapped;
-//     do {
-//         swapped = false;
-//         for (let i = 0; i < n - 1; i++) {
-//             if (arr[i] > arr[i + 1]) {
-//                 let temp = arr[i];
-//                 arr[i] = arr[i + 1];
-//                 arr[i + 1] = temp;
-//                 swapped = true;
-//             }
-//         }
-//         n--;
-//     } while (swapped);
-//     return arr;
-// }
-
-// console.log(bubbleSort(numbers).join(' '))
-
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().trim().split('\n');
-
-// 변수 선언 및 입력
-const n = Number(input[0]);
-const arr = input[1].split(' ').map(Number);
-
-function bubbleSort() {
-    let isSorted = false;
-    while (!isSorted) {
-        isSorted = true;
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+    do {
+        swapped = false;
         for (let i = 0; i < n - 1; i++) {
             if (arr[i] > arr[i + 1]) {
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
-                isSorted = false;
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
             }
         }
-    }
+        n--;
+    } while (swapped);
+    return arr;
 }
 
-bubbleSort();
-
-// 정렬된 배열 요소 출력
-console.log(arr.join(' '));
+console.log(bubbleSort(numbers).join(' '))
