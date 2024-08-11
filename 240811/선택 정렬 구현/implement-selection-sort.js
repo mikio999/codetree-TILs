7b4e2c9a7c576c683a9cba294ddfbeb52e1e3293
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-11 15:40:19 +0900

    Title: 선택 정렬 구현, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240811/선택 정렬 구현/implement-selection-sort.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(`\n`)
+
+const n = input[0]
+let arr = input[1].split(' ').map(Number)
+
+for (let i = 0; i < n-1; i++) {
+    let min = i;
+    for (let j = i+1; j < n-1; j++) {
+        if (arr[j] < arr[min]) {
+            min = j
+        }
+    }
+    let tmp = arr[i]
+    arr[i] = arr[min]
+    arr[min] = tmp
+}
+
+console.log(arr.join(' '))
