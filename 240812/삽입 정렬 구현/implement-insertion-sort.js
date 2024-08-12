ff0d014c9aa0b954e81433a9902e17b0cb26bd25
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-12 09:30:25 +0900

    Title: 삽입 정렬 구현, Time: 234ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240812/삽입 정렬 구현/implement-insertion-sort.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const input = fs.readFileSync(0).toString().trim().split(`\n`);
+const n = input[0]
+const arr = input[1].split(' ').map(Number)
+
+for (let i = 1; i < n; i++ ) {
+    let key = arr[i];
+    j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+        arr[j+1] = arr[j]
+        arr[j] = key;
+        j--;
+        arr[j+1] = key;
+    }
+}
+
+console.log(arr.join(' '))
