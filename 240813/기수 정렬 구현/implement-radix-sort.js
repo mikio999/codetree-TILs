d4b37293768cdbb7de7872b5d26465d5713e4460
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-13 10:33:31 +0900

    Title: 기수 정렬 구현, Time: 5022ms, Memory: 56MB, Status: Time Limit Exceed - CodeTree

---
+++ 240813/기수 정렬 구현/implement-radix-sort.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split(`\n`)
+const n = input[0];
+const arr = input[1].split(' ').map(Number);
+
+const getDigit = (num, target) => {
+    return Math.floor(Math.abs(num)/(10**target))%10
+}
+
+const radixSort = (n,arr) => {
+    for (let k = 0; k < n; k++) {
+        let digitBuckets = Array.from({length:10}, () => []);
+        for (let i=0; i<arr.length;i++) {
+            let digit = getDigit(arr[i], k);
+            digitBuckets[digit].push(arr[i])
+        }
+        arr = [].concat(...digitBuckets)
+    }
+    return arr
+}
+
+console.log(radixSort(n,arr).join(' '))
