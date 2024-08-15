2151a51aad68432c48431c665c71c73934112707
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-15 14:46:02 +0900

    Title: hashmap 기본, Time: 650ms, Memory: 39MB, Status: Passed - CodeTree

--- 240815/hashmap 기본/hashmap-basic.js
+++ 240815/hashmap 기본/hashmap-basic.js
@@ -2,31 +2,23 @@ const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split(`\n`)
 
 const n = Number(input[0])
-
-let commands = []
-for (let i = 1; i < n+1; i++) {
-    let command = input[i].split(' ')
-    commands.push(command)
-}
-
 const m = new Map();
 
-for (let j=0; j<n; j++) {
-    if (commands[j][0] === 'add') {
-        m.set(Number(commands[j][1]), Number(commands[j][2]))
+for (let i = 1; i < n+1; i++) {
+    const [command, k, v] = input[i].split(' ')
+
+    if (command === 'add') {
+        m.set(k,v);
     }
-    else if (commands[j][0] === 'find') {
-        if (m.has(Number(commands[j][1]))) {
-            console.log(m.get(Number(commands[j][1])))
-        }
-        else if(!m.has(Number(commands[j][1]))) {
+    else if (command === 'remove') {
+        m.delete(k);
+    }
+    else if (command === 'find') {
+        if (!m.has(k)) {
             console.log('None')
         }
-
-    }
-    else if (commands[j][0] === 'remove') {
-        if (m.has(Number(commands[j][1]))) {
-            m.delete(Number(commands[j][1]))
+        else {
+            console.log(m.get(k))
         }
     }
 }
