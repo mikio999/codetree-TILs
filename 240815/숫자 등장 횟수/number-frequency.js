f1bac9c6f44d8413bdf031e8f65a4f6c508c8a2b
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-15 15:33:29 +0900

    Title: 숫자 등장 횟수, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240815/숫자 등장 횟수/number-frequency.js
+++ 240815/숫자 등장 횟수/number-frequency.js
@@ -5,21 +5,21 @@ const [n,m] = input[0].split(' ').map(Number)
 const numbers = input[1].split(' ').map(Number)
 const questions = input[2].split(' ').map(Number)
 
-const k = new Map()
-for (let i=0; i < n; i++) {
-    k.set(numbers[i], i+1);
-}
-
-let result = []
-for (let j=0; j < m; j++) {
-    let target = questions[j];
-    let tmp = 0
-    for (let i=0; i<n; i++) {
-        if (target === numbers[i]) {
-            tmp += 1;
+const countNums = (arr, target) => {
+  const k = new Map()
+    for (let i=0; i < n; i++) {
+        if (k.has(numbers[i])) {
+            k.set(numbers[i], k.get(numbers[i]) + 1)
+        }
+        else {
+            k.set(numbers[i], 1)
         }
     }
-   result.push(tmp)
+    return (k.get(target) || 0)
 }
 
-console.log(result.join(' '))
+
+
+for (let j=0; j<m; j++) {
+    console.log(countNums(numbers, questions[j]))
+}
