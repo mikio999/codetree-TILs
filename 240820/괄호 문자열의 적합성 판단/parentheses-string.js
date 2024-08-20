52b6aedfb6c953f015fae8c6d2fbf812b61bdca0
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-08-20 09:46:05 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 169ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240820/괄호 문자열의 적합성 판단/parentheses-string.js
+++ 240820/괄호 문자열의 적합성 판단/parentheses-string.js
@@ -1,23 +1,49 @@
 const fs = require('fs')
 const commands = fs.readFileSync(0).toString().trim().split('')
 
-let stack = []
-
-commands.forEach((item) => {
-    if (item === '(') {
-        stack.push('(')
+class Stack {
+    constructor() {
+        this.items = []
     }
-    else {
-        if (stack[stack.length-1] === '(') {
-            stack.pop()
-        }
-        else {
-            stack.push(')')
+    push(item) {
+        this.items.push(item)
+    }
+    empty() {
+        return this.items.length === 0;
+    }
+    size() {
+        return this.items.length;
+    }
+    pop() {
+        if (this.empty()) {
+            throw new Error('Stack is empty')
         }
     }
-})
+    top() {
+        if (this.empty()) {
+            throw new Error ('Stack is empty')
+        }
 
-if (stack.length === 0) {
+        return this.items[this.items.length - 1]
+    }
+}
+
+const s = new Stack();
+
+for (let char of commands) {
+    if (char === '(') {
+        s.push('(')
+    } else {
+        if (s.empty()) {
+            console.log('No');
+            process.exit(0)
+        }
+
+        s.pop()
+    }
+}
+
+if (s.empty()) {
     console.log('Yes')
 }
 else {
