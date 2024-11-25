c7086f5943b07cd8fb975347dfe7c8f1d9f8a883
Minkyung Kwak <96870855+mikio999@users.noreply.github.com>
2024-11-25 17:15:26 +0900

    Title: 바이러스 검사, Time: 680ms, Memory: 89MB, Status: Passed - CodeTree

--- 241125/바이러스 검사/virus-detector.js
+++ 241125/바이러스 검사/virus-detector.js
@@ -1,27 +1,33 @@
-const fs = require("fs");
-const input = fs.readFileSync(0).toString().split(`\n`);
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-const n = Number(input[0]);
-const peopleInStores = input[1].split(" ").map(Number);
-const capacities = input[2].split(" ");
-const maxLeaderCapacity = Number(capacities[0]);
-const maxMemberCapacity = Number(capacities[1]);
+const input = [];
+rl.on('line', function(line) {
+  input.push(line);
+}).on('close', function() {
+  const n = Number(input[0]);
+  const peopleInStores = input[1].split(' ').map(Number);
+  const capacities = input[2].split(' ');
+  const maxLeaderCapacity = Number(capacities[0]);
+  const maxMemberCapacity = Number(capacities[1]);
 
-let totalCount = 0;
-
-for (let i = 0; i < n; i++) {
-  if (peopleInStores[i] <= maxLeaderCapacity) {
-    totalCount += 1;
-  } else {
-    const peopleManagedByMembers = peopleInStores[i] - maxLeaderCapacity;
-    const fullMemberGroups = Math.floor(
-      peopleManagedByMembers / maxMemberCapacity
-    );
-    totalCount += 1 + fullMemberGroups;
-    if (peopleManagedByMembers % maxMemberCapacity > 0) {
+  let totalCount = 0;
+  for (let i = 0; i < n; i++) {
+    if (peopleInStores[i] <= maxLeaderCapacity) {
       totalCount += 1;
+    } else {
+      const peopleManagedByMembers = peopleInStores[i] - maxLeaderCapacity;
+      const fullMemberGroups = Math.floor(peopleManagedByMembers / maxMemberCapacity);
+      totalCount += 1 + fullMemberGroups;
+      if (peopleManagedByMembers % maxMemberCapacity > 0) {
+        totalCount += 1;
+      }
     }
   }
-}
 
-console.log(totalCount);
+  console.log(totalCount);
+  process.exit();
+});
